5ba590417e37ac211bfee9b4fa9c60b969498b3b
coolpepper <izzy@izzywatson.com>
2021-11-08 21:21:49 -0500

    Remove double period.

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
     <footer>
-      Created by <a href="https://www.izzyhub.com">Izzy</a>. <a href="https://github.com/izzyhub/discord-time-codes">This is site is open source.</a>.
+      Created by <a href="https://www.izzyhub.com">Izzy</a>. <a href="https://github.com/izzyhub/discord-time-codes">This is site is open source</a>.
     </footer>
   );
 }
